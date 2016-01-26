54ed0e00b80e7c3ec4d445271fc442e436b1d6ff
Adam <theadam@gmx.de>
2016-01-26 17:17:25 +0100

    feat: added dummy content policastation pins

--- scripts/map.js
+++ scripts/map.js
@@ -1,11 +1,9 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
+    zoom: 14
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
 
-  // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
@@ -20,8 +18,44 @@ function initMap() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
-    // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  setMarkers(map);
+} 
+
+var policeStations = [
+  ['P1', 47.809574, 13.042736, 1],
+  ['P2', 47.816685, 13.056705, 2],
+  ['P3', 47.813515, 13.062958, 4],
+  ['P4', 47.810377, 13.053259, 2],
+  ['P5', 47.818133, 13.044976, 1],
+  ['P6', 47.836288, 13.012704, 1]
+];
+
+function setMarkers(map) {
+  var image = {
+    url: 'img/POLIZEI_PIN.png',
+    size: new google.maps.Size(20,32),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(0,32)
+  };
+
+  var shape = {
+    coords: [1, 1, 1, 20, 18, 20, 18, 1],
+    type: 'poly'
+  };
+  
+  for (var i = 0; i < policeStations.length; i++) {
+    var pol = policeStations[i];
+    var marker = new google.maps.Marker({
+      position: {lat: pol[1], lng: pol[2]},
+      map: map,
+      icon: image,
+      shape: shape,
+      title: pol[0],
+      zIndex: pol[3]
+    });
   }
 }
 
